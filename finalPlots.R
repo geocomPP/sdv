662e261291336ff7e1208e4f524329afdf674d05
Robin <rob00x@gmail.com>
2014-05-20 15:07:36 +0100

    extra plots

--- finalPlots.R
+++ finalPlots.R
@@ -57,11 +57,20 @@ wrld <- c(geom_polygon(aes(group = group), size = 0.1, colour = "black", fill = 
 base + wrld + coord_fixed()
 ggsave("figure/World_Map.pdf")
 
-# ```{r World Shipping with raster background}
+# ```{r World Shipping}
 base + route + wrld + theme(panel.background = element_rect(fill='#BAC4B9',colour='black')) + 
   annotation_raster(btitle, xmin = 30, xmax = 140, ymin = 51, ymax = 87) + 
   annotation_raster(compass, xmin = 65, xmax = 105, ymin = 25, ymax = 65) + coord_equal() + quiet
-ggsave("figure/World_Shipping_with_raster_background.pdf")
+ggsave("/tmp/figure/World_Shipping.pdf")
+
+# ```{r World Shipping with raster background}
+earth <- readPNG("figure/earth_raster.png")
+base + annotation_raster(earth, xmin = -180, xmax = 180, ymin = -90, ymax = 90) + 
+  route + theme(panel.background = element_rect(fill='#BAC4B9',colour='black')) + 
+  annotation_raster(btitle, xmin = 30, xmax = 140, ymin = 51, ymax = 87) + 
+  annotation_raster(compass, xmin = 65, xmax = 105, ymin = 25, ymax = 65) + 
+  coord_equal() + quiet
+ggsave("/tmp/figure/World_Shipping_with_raster_background.pdf")
 
 
 
